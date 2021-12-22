c64d14b3b40942496f77cd20228b11482bac0b5e
Maëlle Le Herissé <maelle.le-herisse@airbus.com>
2021-12-22 17:15:06 +0100

    cdk: Add AccountFactory construct to global stack
    Update packages

--- lib/awsAccountFactoryStack.ts
+++ lib/awsAccountFactoryStack.ts
@@ -14,16 +14,21 @@ export class AwsAccountFactoryStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AwsAccountFactoryStackProps) {
     super(scope, id, props)
 
-    // The code that defines your stack goes here
-    const managementAccount = new ManagementAccount(this, 'ManagementAccount', {
+    new ManagementAccount(this, 'ManagementAccount', {
       budgetEmail: props.email,
       budgetName: 'ManagementBudget',
       organizationId: props.organizationId
     })
 
-    const accountFactory = new AccountFactory(scope, 'AccountFactory', {})
     const organizationUnits = new OrganizationUnits(this, 'OrganizationUnits', {
       rootOrganizationId: props.rootOu
     })
+
+    const accountFactory = new AccountFactory(this, 'AccountFactory', {
+      rootOU: props.rootOu,
+      sandboxOU: organizationUnits.sandboxOu.ouId, 
+      email: props.email
+    })
+    accountFactory.node.addDependency(organizationUnits)
   }
 }
